b0d7c938a4cca8878a1d13cd29251c561cdad071
yuta <kuro1152@yahoo.co.jp>
2024-01-24 22:09:25 +0900

    study

--- 自習用.sql
+++ 自習用.sql
@@ -9,3 +9,31 @@ retu1 char(10) NOT NULL
 DROP TABLE test;
 
 DROP DATABASE test;
+
+
+-- 復習
+
+-- select文で取り出す
+select
+shohin_id as "商品ID"
+,shohin_mei as "商品名"
+from
+shohin;
+
+select 
+shohin_mei as "商品名"
+,hanbai_tanka as "価格"
+from
+shohin;
+
+-- 重複を除いて取り出す
+select distinct 
+shohin_bunrui as "分類"
+from 
+shohin;
+
+-- 登録日を出力
+select distinct 
+torokubi as "登録日"
+from
+shohin;
